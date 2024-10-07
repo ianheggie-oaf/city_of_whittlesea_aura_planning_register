ed813dda603786fe99ea42564a317ee1a8ca48ce
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2024-10-08 06:13:24 +1100

    experiment

--- capybara_utilities.rb
+++ capybara_utilities.rb
@@ -1,6 +1,7 @@
 # Handle the interface with Capybara and chrome in these methods
 
 require 'net/http'
+require 'webdrivers/chromedriver'
 
 module CapybaraUtilities
   def get_chrome_version
@@ -20,32 +21,40 @@ module CapybaraUtilities
     end
   end
 
-  def create_capybara_session(retry_attempt: false)
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--headless') if headless
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--enable-logging --v=1 --log-path=/tmp/chrome_debug.log')
+  def find_driver_path
+    # Force resolution of the driver path and possible update now to avoid triggering VCR
+    ::Webdrivers::Chromedriver.update
+  rescue ::Webdrivers::VersionError => ex
+    raise unless ex.message =~ /Unable to find latest point release version for (\d+)\./
+    $1.to_i.downto(100) do |major_ver|
+      uri = URI("https://chromedriver.storage.googleapis.com/LATEST_RELEASE_#{major_ver}")
+      res = Net::HTTP.get_response(uri)
+      next unless res.is_a?(Net::HTTPSuccess)
 
-    # if retry_attempt
-    #   options.add_argument('--disable-gpu')
-    #   options.add_argument('--remote-debugging-port=9222')
-    #   options.add_argument('--disable-extensions')
-    #   options.add_argument('--disable-setuid-sandbox')
-    #   options.add_argument('--no-first-run')
-    #   options.add_argument('--no-default-browser-check')
-    #   options.add_argument('--ignore-certificate-errors')
-    #   options.add_argument('--start-maximized')
-    # end
-
-    driver_name = retry_attempt ? :custom_chrome_retry : :custom_chrome
-    browser_type = determine_browser_type
-
-    Capybara.register_driver driver_name do |app|
-      Capybara::Selenium::Driver.new(app, browser: browser_type, options: options)
+      ver = res.body.chomp
+      puts "Using chromdriver version: #{ver} due to: #{ex.message}"
+      Webdrivers::Chromedriver.required_version = ver
+      return ::Webdrivers::Chromedriver.update
     end
+    nil
+  end
 
-    Capybara::Session.new(driver_name)
+  def setup_webdriver
+    driver_path = find_driver_path
+    puts "Using chrome driver path: #{driver_path.inspect} ..."
+    ::Selenium::WebDriver::Chrome::Service.driver_path = driver_path
+
+    Capybara.register_driver :logging_selenium do |app|
+      # caps = # Selenium::WebDriver::Remote::Capabilities.chrome(loggingPrefs: { browser: 'ALL' })
+      #   Selenium::WebDriver::Remote::Capabilities.chrome("goog:loggingPrefs": { browser: 'ALL' })
+      browser_options = ::Selenium::WebDriver::Chrome::Options.new
+      if ENV['DISPLAY'].nil? || ENV['HEADLESS']
+        browser_options.args << '--headless' # add whatever browser args and other options you need (--headless, etc)
+      end
+      browser_type = determine_browser_type
+      puts "Calling Capybara::Selenium::Driver.new(app, browser: #{browser_type.inspect}, options: #{browser_options.inspect})" # , desired_capabilities: #{caps.inspect})"
+      Capybara::Selenium::Driver.new(app, browser: browser_type, options: browser_options) # , desired_capabilities: caps)
+    end
   end
 
   def determine_browser_type
@@ -55,7 +64,22 @@ module CapybaraUtilities
         return browser.include?('chromium') ? :chromium : :chrome
       end
     end
-    :chrome  # Default to Chrome if we can't determine
+    :chrome # Default to Chrome if we can't determine
+  end
+
+  def create_capybara_session(retry_attempt: false)
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless') if headless
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--enable-logging --v=1 --log-path=/tmp/chrome_debug.log')
+
+    driver_name = if retry_attempt
+                    :logging_selenium
+                  else
+                    headless ? :chrome_headless : :selenium_chrome
+                  end
+    Capybara::Session.new(driver_name)
   end
 
   def visit_url(url, retry_attempt: false)
@@ -65,8 +89,8 @@ module CapybaraUtilities
   rescue Selenium::WebDriver::Error::WebDriverError => e
     if retry_attempt
       error "Chrome failed to start on retry: #{e.message}"
-      error "Chrome logs:"
-      error `cat /tmp/chrome_debug.log`
+      # error "Chrome logs:"
+      # error `cat /tmp/chrome_debug.log`
     end
     raise e
   end
@@ -75,22 +99,9 @@ module CapybaraUtilities
     visit_url(url)
   rescue Selenium::WebDriver::Error::WebDriverError => e
     error "Error visiting URL: #{e.message}"
-    info "Attempting to update ChromeDriver with default version and retry..."
-    require 'webdrivers'
-    begin
-      Webdrivers::Chromedriver.update
-      visit_url(url, retry_attempt: true)
-    rescue Selenium::WebDriver::Error::WebDriverError, Webdrivers::VersionError => e
-      error "Error retrying to visit URL: #{e.message}"
-      info "Attempting to determine required chromedriver to update ChromeDriver and retry..."
-      chrome_version = get_chrome_version
-      info "Found chrome version: #{chrome_version}"
-      chromedriver_version = find_latest_compatible_chromedriver(chrome_version)
-      info "Found compatible ChromeDriver version: #{chromedriver_version}"
-      Webdrivers::Chromedriver.required_version = chromedriver_version if chromedriver_version
-      Webdrivers::Chromedriver.update
-      visit_url(url, retry_attempt: true)
-    end
+    info "Attempting to update ChromeDriver and retry..."
+    setup_webdriver
+    visit_url(url, retry_attempt: true)
   end
 
   def find_latest_compatible_chromedriver(chrome_version)

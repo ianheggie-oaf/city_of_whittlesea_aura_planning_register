dd6939b190c2c7a5831093f0f6daba4b328e89b6
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2024-10-08 05:44:35 +1100

    retry with same options

--- capybara_utilities.rb
+++ capybara_utilities.rb
@@ -27,16 +27,16 @@ module CapybaraUtilities
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--enable-logging --v=1 --log-path=/tmp/chrome_debug.log')
 
-    if retry_attempt
-      options.add_argument('--disable-gpu')
-      options.add_argument('--remote-debugging-port=9222')
-      options.add_argument('--disable-extensions')
-      options.add_argument('--disable-setuid-sandbox')
-      options.add_argument('--no-first-run')
-      options.add_argument('--no-default-browser-check')
-      options.add_argument('--ignore-certificate-errors')
-      options.add_argument('--start-maximized')
-    end
+    # if retry_attempt
+    #   options.add_argument('--disable-gpu')
+    #   options.add_argument('--remote-debugging-port=9222')
+    #   options.add_argument('--disable-extensions')
+    #   options.add_argument('--disable-setuid-sandbox')
+    #   options.add_argument('--no-first-run')
+    #   options.add_argument('--no-default-browser-check')
+    #   options.add_argument('--ignore-certificate-errors')
+    #   options.add_argument('--start-maximized')
+    # end
 
     driver_name = retry_attempt ? :custom_chrome_retry : :custom_chrome
     browser_type = determine_browser_type
